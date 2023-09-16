cf0d2fd1b336595e29daeb99bc7e3069694a2650
William Krasnov <willko@macbook-pro.myfiosgateway.com>
2023-09-16 19:12:50 -0400

    Initial Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">
-      Hello Discord Clone 😄
-    </p>
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">
+        Hello Discord Clone 😄
+
+      </p>
+      <Button >Click me</Button>
+    </div>
   )
 }
