d264d333a1f66bebb22ae616b692d9ae7e73dd7e
William Krasnov <willkoman@gmail.com>
2023-09-16 20:03:32 -0400

    Signin/up configured via Clerk

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
+  )
+}
+
